8d4b868da472f1b1acfb4fe7db85fb6bbbfbc2f6
franquitt <francoagresta96@hotmail.com>
2012-10-17 21:16:29 -0300

    Update pasando_a_pilas.py

--- pasando_a_pilas.py
+++ pasando_a_pilas.py
@@ -1,8 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+import thread
+from threading import Thread
+import wx
+import time
 import pilas
 pilas.iniciar(ancho = 900, alto = 600)
-entrada_enviar = pilas.interfaz.IngresoDeTexto(ancho=350)
-entrada_enviar.y = -280
-boton_enviar = pilas.interfaz.Boton("Enviar")
-boton_enviar.y = -280
-boton_enviar.x = 220
+class ClienteP():#anteriormente class Cliente(object), adaptado para correr en otro hilo
+    def __init__(self):#se piden los parametros: el host y el objeto donde se mostarÃ¡n las conversaciones
+        self.enviar = pilas.interfaz.IngresoDeTexto(ancho=350, limite_de_caracteres=48)
+        self.enviar.y = -280
+        self.boton_enviar = pilas.interfaz.Boton("Enviar")
+        self.boton_enviar.y = -280
+        self.boton_enviar.x = 220
+        self.conectar = pilas.interfaz.IngresoDeTexto(ancho=350, limite_de_caracteres=48)
+        self.conectar.y = 280
+        self.boton_conectar = pilas.interfaz.Boton("Conectar")
+        self.boton_conectar.y = 280
+        self.boton_conectar.x = 220
+        def enviar():                
+                aponer = str(self.enviar.texto)
+                hola = pilas.actores.Texto(aponer)
+        self.boton_enviar.conectar(enviar)
+        #self.boton_conectar.conectar(self.conectar)
+                
+
+
+        #def conectar(self):
+                        
+
+cliente = ClienteP()
 pilas.ejecutar()
